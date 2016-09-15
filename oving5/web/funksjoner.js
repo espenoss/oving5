44fcdd12f6bca2d00642f5b1edeefd25f90930c1
MariaShchekaneko <mariya.shchekanenko@gmail.com>
2016-09-15 17:02:47 +0200

    html

--- oving5/web/funksjoner.js
+++ oving5/web/funksjoner.js
@@ -1,7 +1,42 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+            $(document).ready(function(){
+                
+                
+                $('Table').DataTable( {
+                "order": [[ 0, "asc" ]],
+                        ajax: {
+                        url: 'rest/sykler',
+                        dataSrc: '',
+                        
+                    },
+                    columns: [
+                        { data: 'plassering' },
+                        { data: 'batteriStatus' },
+                        { data: 'ledig' },
+                        { data: 'nr' }
+                        
+                    ]
+                });
+                
+                
+                $("#create").click(function () {
+                    $.ajax({
+                        url: 'rest/sykler',
+                        type: 'POST',
+                        data: JSON.stringify({
+                            plassering: $("#plassering").val(),
+                            batteriStatus: $("#batteriStatus:").val(),
+                            ledig: $("#ledig").val(),
+                            nr:$("#nr").val(),
+                            
 
+                            
+                        }),
+                        contentType: 'application/json; charset=utf-8',
+                        dataType: 'json',
+                        success: function(result) {
+                            $('#myTable').DataTable().ajax.reload();
+                        }
+                    });
+                });
+            });
