02957aa2e95ab61204c9b11f7be9273fe6b8f819
Ein fugel <Ein fugel@DESKTOP-I8IGRE1>
2016-09-21 19:02:51 +0200

    øving6

--- oving5/src/java/minpakke/SykkelService.java
+++ oving5/src/java/minpakke/SykkelService.java
@@ -5,28 +5,106 @@ import java.util.Random;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("/sykkelService/sykler")
+@Path("/sykkelService/")
 public class SykkelService {
     
     
     private static ArrayList<Sykkel> sykler = new ArrayList<Sykkel>(){{
         add(new Sykkel("1", "Dragvoll"));
-        add(new Sykkel("2", "Glos"));
+        add(new Sykkel("2", "Gløshaugen"));
     }};
-     
+    
+    private static ArrayList<Kunde> kunder = new ArrayList<Kunde>(){{
+       add(new Kunde("espen", "e@google.no"));
+       add(new Kunde("maria", "@hotmail.ru"));
+    }};
   
-   @PUT
+    @PUT
+    @Path("/kunder/")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.TEXT_PLAIN})
+    public String leggTilKunde(Kunde kunde){
+        for(Kunde k: kunder){
+            if(k.getBrukerNavn().equals(kunde.getBrukerNavn())){
+                return "Brukernavn eksisterer allerede";
+            }
+        }
+        kunder.add(kunde);
+        return "Kunde registrert";
+    }
+    
+    @GET
+    @Path("/kunder/")    
+    @Produces({MediaType.APPLICATION_JSON})
+    public ArrayList<Kunde> getKunder(){
+        return kunder;
+    }
+    
+    @GET
+    @Path("/kunder/{brukerNavn}/")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Kunde getKunde(@PathParam("brukerNavn") String brukerNavn){
+        
+        for(Kunde k:kunder){
+            if(k.getBrukerNavn().equals(brukerNavn)){
+                return k;
+            }
+        }
+        throw new NotFoundException();
+    }
+    
+    @POST
+    @Path("/kunder/{brukerNavn}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    public String leggTilBestilling(@PathParam("brukerNavn") String brukerNavn, Sykkel sykkel){
+        
+        Kunde kunde = null;
+        
+        for(Kunde k:kunder){
+            if(k.getBrukerNavn().equals(brukerNavn)){
+                kunde = k;
+            }
+        }
+        if(kunde == null){
+            throw new NotFoundException();            
+        }
+        
+        int kode = new Random().nextInt(1000);
+        String kodeString = Integer.toString(kode);
+        
+        String dato = new java.util.Date().toString();
+
+        kunde.getBestillinger().add(new Bestilling(dato, kodeString, sykkel));
+        
+        return kodeString;
+    }
+    
+    @GET
+    @Path("kunder/{brukerNavn}/bestillinger")
+    @Produces({MediaType.APPLICATION_JSON})
+    public ArrayList<Bestilling> getBestillinger(@PathParam("brukerNavn") String brukerNavn){
+        
+        for(Kunde k:kunder){
+            if(k.getBrukerNavn().equals(brukerNavn)){
+                return k.getBestillinger();
+            }
+        }
+        throw new NotFoundException();
+    }
+    
+    @PUT
+    @Path("/sykler/")
     @Consumes({MediaType.TEXT_PLAIN})
     public void addSykkel(String plassering){
         int sisteNummer = Integer.parseInt(sykler.get(sykler.size()-1).getNr()); // Hent siste nummer
-        String nyttNr = Integer.toString(++sisteNummer).toString();              // inkrementer med en og konverter til string
+        String nyttNr = Integer.toString(++sisteNummer);              // inkrementer med en og konverter til string
         Sykkel nySykkel= new Sykkel(nyttNr, plassering);
         sykler.add(nySykkel);
     }
     
     @POST
-    @Path("/{sykkelNr}/reserver")
-    @Consumes({MediaType.APPLICATION_JSON})
+    @Path("/sykler/{sykkelNr}/reserver")
     public void reserverSykkel(@PathParam("sykkelNr") String sykkelNr){
         for(Sykkel s: sykler){
             if(s.getNr().equals(sykkelNr)){
@@ -38,13 +116,26 @@ public class SykkelService {
     }
 
     @POST
-    @Path("/{sykkelNr}/lever")
-    @Consumes({MediaType.APPLICATION_JSON})
-    public void leverSykkel(@PathParam("sykkelNr") String sykkelNr, String plassering){
+    @Path("/sykler/{sykkelNr}/lever")
+    @Consumes({MediaType.TEXT_PLAIN})
+    @Produces({MediaType.TEXT_PLAIN})
+    public String leverSykkel(@PathParam("sykkelNr") String sykkelNr, String plassering){
         for(Sykkel s: sykler){
             if(s.getNr().equals(sykkelNr)){
                 s.setLedig("Ledig");
                 s.setPlassering(plassering);
+                return "Sykkel levert";
+            }
+        }
+        return "Sykkelnummer ikke funnet";
+    }
+    
+    @POST
+    @Path("/sykler/{sykkelNr}/hent")
+    public void hentSykkel(@PathParam("sykkelNr") String sykkelNr){
+        for(Sykkel s: sykler){
+            if(s.getNr().equals(sykkelNr)){
+                s.setPlassering("Ute");
                 return;
             }
         }
@@ -52,13 +143,14 @@ public class SykkelService {
     }
     
     @GET
+    @Path("/sykler/")
     @Produces({MediaType.APPLICATION_JSON})
     public ArrayList<Sykkel> getSykler(){
         return sykler;
     }    
     
     @GET
-    @Path("/{plassering}")
+    @Path("/sykler/{plassering}")
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     public Sykkel getLedigSykkel(@PathParam("plassering") String plassering){
--- oving5/web/funksjoner.js
+++ oving5/web/funksjoner.js
@@ -2,14 +2,15 @@
             $(document).ready(function(){
                 
                 
-                $('#Table').DataTable( {
-                "order": [[ 0, "asc" ]],
+                $('#sykkelTable').DataTable( {
+                "order": [[ 1, "asc" ]],
                         ajax: {
                         url: 'rest/sykkelService/sykler',
                         dataSrc: '',
                         
                     },
                     columns: [
+                        { data: 'nr'},
                         { data: 'plassering' },
                         { data: 'batteriStatus' },
                         { data: 'ledig' }
@@ -45,79 +46,14 @@
                     ]
                 });                
                 
+           
+
                 
-                $("#create").click(function () {
-                    $.ajax({
-                        url: 'rest/sykkelService',
-                        type: 'POST',
-                        data: JSON.stringify({
-                            plassering: $("#plassering").val(),
-                            batteriStatus: $("#batteriStatus:").val(),
-                            ledig: $("#ledig").val(),
-                            nr:$("#nr").val(),
-                            
-
-                            
-                        }),
-                        contentType: 'application/json; charset=utf-8',
-                        dataType: 'json',
-                        success: function(result) {
-                            $('#myTable').DataTable().ajax.reload();
-                        }
-                    });
-                }); 
-                
-                $("#booking").click(function () {
-                    $.ajax({
-                        url: 'rest/sykkelService',
-                        type: 'POST',
-                        data: JSON.stringify({
-                            navn: $("#navn").val(),
-                            email: $("#email:").val(),
-                            parkering: $("#parkering").val(),   
-                        }),
-                        contentType: 'application/json; charset=utf-8',
-                        dataType: 'json',
-                        success: function(result) {
-                            $('#myTable2').DataTable().ajax.reload();
-                        }
-                    });
-                });
-
-
                 $("#reserver").click(function(){
-                    var kunde;
-                    
-                    $.get("rest/sykkelService/kunder/espen", function(data){
-                            kunde = data;
-                            var sykkel;
-                            $.get("rest/sykkelService/sykler/Dragvoll", function(data){
-                                sykkel = data;
-                                $.ajax({
-                                    url: 'rest/sykkelService/kunder/' + kunde.brukerNavn,
-                                    type: 'POST',
-                                    data: JSON.stringify(sykkel),
-                                    contentType: 'application/json',
-                                    dataType: 'json',
-                                    success: function(){
-                                        $.ajax({
-                                             url: 'rest/sykkelService/sykler/' + sykkel.nr + '/reserver',
-                                            type: 'POST',
-                                            success: function(){
-                                                $('#Table').DataTable().ajax.reload();
-                                            }
-                                        });
-                                    }
-                                });
-                        });
-                    });
-                });
-                
-                $("#reserver2").click(function(){
                     brukerNavn = $("#brukerNavn").val();
                     plassering = $("#plassering").val();
                     
-                    $.get("rest/sykkelService/kunder/" + brukerNavn, function(data){
+                    $.get("rest/sykkelService/kunder/" + brukerNavn, function(data){            
                             kunde = data;
                             var sykkel;
                             $.get("rest/sykkelService/sykler/" + plassering, function(data){
@@ -133,7 +69,8 @@
                                              url: 'rest/sykkelService/sykler/' + sykkel.nr + '/reserver',
                                             type: 'POST',
                                             success: function(){
-                                                $('#Table').DataTable().ajax.reload();
+                                                $('#sykkelTable').DataTable().ajax.reload();
+                                                $('bestillingTable').DataTable().ajax.reload();
                                             }
                                         });
                                     }
@@ -147,11 +84,50 @@
                    registrerSykkel(plassering);
                    
                 });
+                
+                $("#registrerBruker").click(function () {
+                    $.ajax({
+                        url: 'rest/sykkelService/kunder',
+                        type: 'PUT',
+                        data: JSON.stringify({
+                            brukerNavn: $("#nyBrukerNavn").val(),
+                            epost: $("#nyBrukerEpost").val(),
+                            bestillinger: []
+                        }),
+                        contentType: 'application/json; charset=utf-8',
+                        dataType: 'json',
+                        success: function(result) {
+                            $('#kundeTable').DataTable().ajax.reload();
+                        }
+                    });
+                });
+                
+                $("#hentSykkel").click(function(){
+                    sykkelNr = $('#hentSykkelNr').val();
+                    $.ajax({
+                        url: 'rest/sykkelService/sykler/' + sykkelNr + '/hent',
+                        type: 'POST',
+                        success: function(){
+                            $("#sykkelTable").DataTable().ajax.reload();                            
+                        }
+                    });
+                });
+                
+                $("#leverSykkel").click(function(){
+                    sykkelNr = $('#leverSykkelNr').val();
+                    sykkelPlassering = $('#leverSykkelPlassering').val();
+                    $.ajax({
+                        url: 'rest/sykkelService/sykler/' + sykkelNr + '/lever',
+                        type: 'POST',
+                        data: sykkelPlassering,
+                        contentType: 'text/plain',
+                        success: function(){
+                            $("#sykkelTable").DataTable().ajax.reload();                            
+                        }
+                    });
+                });
+                
             });
-
-            function hentKunde(kunde){
-                $.get("rest/sykkelService/kunder/" + kunde);
-            }
 
             function registrerSykkel(plassering){
                 $.ajax({
@@ -159,7 +135,11 @@
                     type: 'PUT',
                     data: plassering,
                     dataType: 'text',
-                    contentType: 'text/plain'});
+                    contentType: 'text/plain',
+                    success: function(){
+                        $("#sykkelTable").DataTable().ajax.reload();
+                    }
+                });
             }
             
             
